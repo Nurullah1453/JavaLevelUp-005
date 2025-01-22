5d1e5a2ba296d6dd3846918adae24cb8950c21e5
NURULLAH DEMIR <EXT02D53329@turkcell.entp.tgc>
2025-01-22 23:33:17 +0300

    fourth commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,28 @@
 import com.workintech.cylinder.Circle;
 import com.workintech.cylinder.Cylinder;
+import com.workintech.pool.Cuboid;
+import com.workintech.pool.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
-       // Circle circle = new Circle(3.75);
-        //System.out.println("circle.radius= " + circle.getRadius());
+        Rectangle rectangle = new Rectangle(5, 10);
 
-       // System.out.println("circle.area= " + circle.getArea());
+        System.out.println("rectangle.width= " + rectangle.getWidth());
 
-       // Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("rectangle.length= " + rectangle.getLength());
 
-       // System.out.println("cylinder.radius= " + cylinder.getRadius());
+        System.out.println("rectangle.area= " + rectangle.getArea());
 
-       // System.out.println("cylinder.height= " + cylinder.getHeight());
-       // System.out.println("cylinder.area= " + cylinder.getArea());
+        Cuboid cuboid = new Cuboid(5,10,5);
 
-       // System.out.println("cylinder.volume= " + cylinder.getVolume());
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+
+        System.out.println("cuboid.length= " + cuboid.getLength());
+
+        System.out.println("cuboid.area= " + cuboid.getArea());
+
+        System.out.println("cuboid.height= " + cuboid.getHeight());
+
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
     }
 }
